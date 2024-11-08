666e11d83ad53b61c96166a466531ed7bb4825f9
chandanshyam <chandanparameshwarappa96@gmail.com>
2024-11-08 20:50:04 +0530

    deployment test11:08:24-08:51pm

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const Stats = lazy(() => import('./pages/Stats'));
 
 const App = () => (
   // basename={PUBLIC_URL}
-  <BrowserRouter>
+  <BrowserRouter basename="/personal-site">
     <Suspense fallback={<Main />}>
       <Routes>
         <Route path="/" element={<Index />} />
