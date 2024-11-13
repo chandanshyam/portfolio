e40226cb71cfa439933fe4e43beb4670f8455689
chandanshyam <chandanparameshwarappa96@gmail.com>
2024-11-14 00:58:52 +0530

    addition of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,33 @@
 // TODO Add a couple lines about each project
+
 const data = [
   {
     title: 'Cloud-Native System for Scalable File Processing',
     subtitle: 'A GCP-powered microservice architecture for real-time data management.',
     date: '2024-10-28',
     desc:
-      'Designed a cloud-native system on GCP with Cloud Run, Pub/Sub, and Firestore to ensure scalability and real-time data processing. '
-      + 'Backend services in NodeJs were containerized with Docker and deployed on Cloud Run, with React handling the frontend. '
+      'Designed a cloud-native system on GCP with Cloud Run, Pub/Sub, and Firestore to ensure scalability and real-time data processing.'
+      + ' Backend services in Node.js were containerized with Docker and deployed on Cloud Run, with React handling the frontend.'
       + 'Implemented role-based access control (RBAC) and fault-tolerant design for security, seamless communication, and workload management.',
   },
   {
     title: 'Campus Event Management System',
     subtitle: 'An automated solution for managing college events and boosting student participation.',
+    date: '2024-10-10',
     desc:
-      'Developed to streamline the organization of events within a college campus. '
-      + 'This full-stack application is powered by Java Spring Boot for the backend, with RESTful APIs managing event registrations, '
-      + 'attendance tracking, and notifications. React serves as the frontend,'
-      + 'Implemented as part of a college project, the platform successfully facilitated multiple events, increasing participation by 30% within the first semester.',
+      'Developed to streamline the organization of events within a college campus.'
+      + 'This full-stack application is powered by Java Spring Boot for the backend, with RESTful APIs managing event registrations, attendance tracking, and notifications.'
+      + 'React serves as the frontend, implemented as part of a college project, successfully facilitating multiple events and increasing participation by 30% within the first semester.',
   },
   {
-    title: 'Campus Event Management System',
-    subtitle: 'An automated solution for managing college events and boosting student participation.',
+    title: 'AI-Powered Email Client',
+    subtitle: 'A next-gen email client leveraging AI for productivity and seamless user experience.',
+    date: '2024-11-10',
     desc:
-      'Developed to streamline the organization of events within a college campus. '
-      + 'This full-stack application is powered by Java Spring Boot for the backend, with RESTful APIs managing event registrations, '
-      + 'attendance tracking, and notifications. React serves as the frontend,'
-      + 'Implemented as part of a college project, the platform successfully facilitated multiple events, increasing participation by 30% within the first semester.',
+      'Led development of an AI-powered email client, prioritizing performance, security, and user-friendly design.'
+      + 'Utilized Next.js 14, React, and TypeScript for efficient server-side rendering and a responsive interface.'
+      + 'Integrated OpenAI’s API for intelligent features, including automated responses and content suggestions to enhance user productivity.'
+      + 'Implemented a secure subscription system with Stripe and webhooks for real-time event handling, enabling access to premium features.',
   },
 ];
 
